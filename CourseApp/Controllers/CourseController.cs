d094257873f4c8b35c2d5fb019dbe8aa7abac90c
UGUR <53657851+ugurkabak35@users.noreply.github.com>
2021-02-04 21:05:36 +0300

    Commit2

--- CourseApp/Controllers/CourseController.cs
+++ CourseApp/Controllers/CourseController.cs
@@ -9,9 +9,20 @@ namespace CourseApp.Controllers
 {
     public class CourseController:Controller
     {
-        public ActionResult Details(int id)
+        //Course/Details/2 : route
+        // Course/Details?id=2
+        public ActionResult Details(int courseid, string sortby)
         {
-            return Content("id = " + id);
+            return Content("id = " + courseid+" sortby :"+sortby);
+        }
+        public ActionResult CourseList(int? pageindex, string sortby)
+        {
+            if (!pageindex.HasValue)
+                pageindex = 1;
+            if (string.IsNullOrWhiteSpace(sortby))
+                sortby = "name";
+
+            return Content("pageindex = " + pageindex + " sortby : " + sortby);
         }
         
 
